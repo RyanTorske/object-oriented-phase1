12a675c93087528fc9367e4c6d3a14675e4838f4
Ryan Torske <rtorske@cnm.edu>
2020-01-23 20:49:28 -0700

    Extra spacing added to easily view, finished class variables.

--- php/Classes/Foo.php
+++ php/Classes/Foo.php
@@ -1,25 +1,48 @@
 <?php
 /**
- * Author of a News Website
+ * Author of a News Website logging into account
  *
  * The author class will be where all data will be kept about author.
  *
  * @author Ryan Torske <rtorske@cnm.edu
  **/
 class author {
-use ???;
+use ValidateUuid;
+use ValidateDate;
+
 /**
  * id for the author and will act as primary key
+ * @var $authorId
  */
 private $authorId;
+
 /**
  * a token that is used to verify the author profile is valid
  * @var $authorActivationToken
  */
 private $authorActivationToken;
+
 /**
- * url for the avatar of the author
+ * url for the avatar or public photo of the author
  * @var $authorAvatarUrl
  */
 private $authorAvatarUrl;
+
+/**
+ * email for the author profile
+ * @var string $authorEmail
+ */
+private $authorEmail;
+
+/**
+ * password for the author login that will be hashed
+ *@var string $authorHash
+ */
+private $authorHash;
+
+/**
+ * username for the author, normally used for a login form
+ * @var $authorUsername
+ */
+private $authorUsername;
 }
